52565953093e6c64b0a065ce2e55b1d9726d001e
joel <joelbong@hotmail.com>
2025-04-19 21:48:32 +0000

    refactor(astro config): :wrench: use env variable for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://staptis.com",
-
+  site: process.env.ASTRO_SITE,
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
