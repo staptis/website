49ca3b3314df00460b67b481458329a1869032d3
joel <joelbong@hotmail.com>
2025-05-23 22:50:44 +0000

    refactor(localization): add for contact page

--- src/utils/i18n-schema.ts
+++ src/utils/i18n-schema.ts
@@ -15,6 +15,15 @@ export const TranslationSchema = z.object({
     terms_of_service: z.string(),
     all_rights_reserved: z.string(),
   }),
+  contact: z.object({
+    get_in_touch: z.string(),
+    name: z.string(),
+    email: z.string(),
+    company_name: z.string(),
+    send_message: z.string(),
+    message_sent: z.string(),
+    error_sending_message: z.string(),
+  }),
 });
 
 export type Translation = z.infer<typeof TranslationSchema>;
